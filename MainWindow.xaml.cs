fcfdf889f5fbd9bc670ae018dd65e18ab4136676
lordnagibator2013 <kekynkek2308@email.com>
2025-10-30 22:23:42 +1000

    begin

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lord13;
+
+public partial class MainWindow : Window
+{
+    int count = 0;
+    string[] listbox = new string[50];
+
+    private void NeGotovo()
+    {
+        MessageBox.Show("не готово(");
+    }
+
+    private void Error()
+    {
+        MessageBox.Show("ты дурачина");
+    }
+
+    // public MainWindow()
+    // {
+    //     InitializeComponent();
+    // }
+}
